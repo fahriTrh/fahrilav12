58cf3745135a27b6042e18b1f629b4c93115e019
fahriTrh <fakhrialauddin44@gmail.com>
2025-10-01 14:57:09 +0700

    menambahkan beberapa route baru di web.php by Fakhri Alauddin Tarihoran

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,37 @@ Route::get('/about', function () {
         <h1>Halaman About</h1>
         <a href='/'>Home</a>
         <a href='/profile'>Profile</a>
+        <a href='/mahasiswa'>Mahasiswa</a>
+        <a href='/contact'>Contact</a>
         ";
-    });
-    
-    Route::get('/profile', function () {
-        return "
+});
+
+Route::get('/profile', function () {
+    return "
         <h1>Halaman Profile</h1>
         <a href='/'>Home</a>
         <a href='/about'>About</a>
+        <a href='/mahasiswa'>Mahasiswa</a>
+        <a href='/contact'>Contact</a>
+    ";
+});
+
+Route::get('/mahasiswa', function () {
+    return "
+        <h1>Halaman Mahasiswa</h1>
+        <a href='/'>Home</a>
+        <a href='/about'>About</a>
+        <a href='/profile'>Profile</a>
+        <a href='/contact'>Contact</a>
+    ";
+});
+
+Route::get('/contact', function () {
+    return "
+        <h1>Halaman Contact</h1>
+        <a href='/'>Home</a>
+        <a href='/about'>About</a>
+        <a href='/profile'>Profile</a>
+        <a href='/mahasiswa'>Mahasiswa</a>
     ";
 });
